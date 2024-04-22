548653252a706424a0a4dbec2ef458ff32f75a43
vishal <vishalninaniya8949info@gmail.com>
2024-04-22 13:59:23 +0530

    daily-sleeps v1 complete

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 import SignupForm from "../ui/forms/signup-form";
-// import { useSession } from "next-auth/react";
-// import { redirect } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  // const { data: session } = useSession();
-  // if (session) {
-  //   return redirect("/home");
-  // }
+  const { data: session } = useSession();
+  if (session) {
+    return redirect("/home");
+  }
   return <SignupForm />;
 }
